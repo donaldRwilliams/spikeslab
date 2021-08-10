b5accae1602deba1da02ca01772478de35332673
donaldRwilliams <drwwilliams@ucdavis.edu>
2021-08-10 08:11:08 -0700

    update

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,14 +2,15 @@ import pytest
 import pyjags
 
 def test_testing():
-      mod =    '''
+    mod =    '''
        model{
            temp ~ dnorm(0, 1)
            }
         '''
+        
+    n_chains = 2
+    coinflip_model = pyjags.Model(mod, chains = n_chains)
+    coinflip_burnin = coinflip_model.sample(500, vars=['temp']) #warmup/burn-in
+    coinflip_samples = coinflip_model.sample(2500, vars=['temp'])
 
-n_chains = 2
-coinflip_model = pyjags.Model(mod, chains = n_chains)
-
-coinflip_burnin = coinflip_model.sample(500, vars=['temp']) #warmup/burn-in
-coinflip_samples = coinflip_model.sample(2500, vars=['temp'])
+test_testing()
